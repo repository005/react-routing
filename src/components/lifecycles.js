011682036ea001e79ef40638defe71f9f8f2e27d
Roman <romansidorenko1990@gmail.com>
2018-09-13 14:57:36 +0300

    lifcycles countinued

--- src/components/lifecycles.js
+++ src/components/lifecycles.js
@@ -7,7 +7,7 @@ class Life extends React.Component {
   //2 get default state
 
   state = {
-    title: 'Lief Cycles'
+    title: 'Life Cycles'
   }
 
   //3 before rendering
@@ -15,19 +15,50 @@ class Life extends React.Component {
     console.log('3 before render');
   }
 
+  componentWillUpdate() {
+    console.log('before update');
+    
+  }
+
+  componentDidUpdate() {
+    console.log('after update');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+
+    if(nextState.title === 'something else') {
+      return false;
+    }
+    
+    return true;
+  }
+
+  componentWillReceiveProps() {
+    console.log('BEFORE RECEIVE PROPS');
+    
+  }
+
+  componentWillUnmount() {
+    console.log('UNMOUNT');
+  }
   //4 render jsx
   render() {
 
-    console.log('4 render');
+    console.log('RENDER');
     
     return(
       <div>
         <h3>{this.state.title}</h3>
+        <div onClick={
+          ()=> this.setState({
+            title: 'something elsee'
+          })}
+          >CLICK TO CHANGE</div>
       </div>
     )
   }
 
-  //5 after jsx
+  //5 after render jsx
   componentDidMount() {
     console.log('5 after render');
     
